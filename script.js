dc9c5ff17af7a4826f0463cb98b27a8a7276c7b9
Muhammad Zaid <muhammadzaid2009@outlook.com>
2026-02-28 23:15:28 +0500

    Implement typed.js for role display
    Added typed.js functionality to display multiple roles.

---
+++ script.js
@@ -0,0 +1,24 @@
+var typed = new Typed("#element", {
+        strings: [
+          "Web Developer",
+          "Graphic Designer",
+          "Freelancer",
+          "Ai Automator",
+          "Content Creator",
+          "Video Editor",
+          "Python Developer",
+        ],
+        typeSpeed: 50,
+      });
+      var typed = new Typed("#element", {
+        strings: [
+          "Web Developer",
+          "Graphic Designer",
+          "Freelancer",
+          "Ai Automator",
+          "Content Creator",
+          "Video Editor",
+          "Python Developer",
+        ],
+        typeSpeed: 50,
+      });
